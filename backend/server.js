0c3fb55b954b83eaf2e6576d567103f041b4df0d
varunjamwal1 <varunjamwal933@gmail.com>
2026-03-20 13:43:25 +0530

    cafestar

--- backend/server.js
+++ backend/server.js
@@ -71,9 +71,11 @@ app.get("/", (req, res) => {
 
 /* ---------------- SERVER ---------------- */
 
-// const PORT = process.env.PORT || 5000;
+if (!process.env.VERCEL) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+}
 
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on port ${PORT}`);
-// });
-export { app };
+export default app;
