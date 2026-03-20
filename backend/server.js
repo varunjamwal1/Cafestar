c5d41aeb9d974f725c25f8be52e99ea6239ae790
varunjamwal1 <varunjamwal933@gmail.com>
2026-03-20 13:06:11 +0530

    cafestar

--- backend/server.js
+++ backend/server.js
@@ -73,8 +73,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on port ${PORT}`);
+// });
 
 export {app}
